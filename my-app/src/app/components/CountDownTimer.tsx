33f8977586eec161f714cfb4dd9efcffbc21adca
danibystrom <124156950+danibystrom@users.noreply.github.com>
2025-06-07 13:38:09 +0200

    refactor CountdownTimer and HeroSection components; update layout and styling

--- my-app/src/app/components/CountDownTimer.tsx
+++ my-app/src/app/components/CountDownTimer.tsx
@@ -1,4 +1,5 @@
 'use client';
+import { Box, Typography } from '@mui/material';
 import { useCallback, useEffect, useState } from 'react';
 
 function CountdownTimer() {
@@ -20,7 +21,7 @@ function CountdownTimer() {
         onTimerEnd();
       }
     };
-  }, [currentTime, targetDate]);
+  }, [currentTime, targetDate, onTimerEnd]);
 
 
   const getTimeRemaining = () => {
@@ -41,14 +42,12 @@ function CountdownTimer() {
   const { days, hours, minutes, seconds } = getTimeRemaining();
 
   return (
-    <div>
-      <div>
-        {days > 0 && <span> days: {days}</span>}
-        {hours > 0 && <span> hours: {hours.toString().padStart(2, '0')}</span>}
-        <span> minutes: {minutes.toString().padStart(2, '0')}:</span>
-        <span> seconds: {seconds.toString().padStart(2, '0')}</span>
-      </div>
-    </div>
+    <Box sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'center' }}>
+      {days > 0 && <Typography sx={{ padding: 0.5 }}>{days} days,</Typography>}
+      {hours > 0 && <Typography sx={{ padding: 0.5 }}>{hours.toString().padStart(2, '0')} hours,</Typography>}
+      <Typography sx={{ padding: 0.5 }}>{minutes.toString().padStart(2, '0')} minutes,</Typography>
+      <Typography sx={{ padding: 0.5 }}>{seconds.toString().padStart(2, '0')} seconds,</Typography>
+    </Box>
   );
 }
 
--- my-app/src/app/components/HeroSection.tsx
+++ my-app/src/app/components/HeroSection.tsx
@@ -1,19 +1,22 @@
 'use client';
 import { Box, Typography } from "@mui/material";
-import CountdownTimer from "./CountDownTimer";
 
 export default function HeroSection() {
     return (
-        <Box sx={{ height: "100vh", display: "flex", justifyContent: "center", alignItems: "center", backgroundImage: "url(/coupleimg.jpg)", backgroundSize: "cover", backgroundPosition: "center" }}>
+        <Box sx={{ height: "100vh", display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", backgroundImage: "url(/coupleimg.jpg)", backgroundSize: "cover", backgroundPosition: "center" }}>
             <Typography
                 variant="h1"
-                sx={{ fontFamily: '"Italiana", sans-serif' }}
+                sx={{ fontFamily: '"Italiana", sans-serif', textAlign: "center", lineHeight: 1, marginBottom: 4 }}
             >
-                SEE YOU THERE
+                SAVE THE DATE
             </Typography>
-
-
-            <CountdownTimer />
+            <Typography
+                variant="h6"
+                sx={{ fontFamily: '"Antic Didone", serif', fontWeight: 100, }}
+            >
+                28 augusti 14.00 | Villa Strömfors 1, Svenljunga
+            </Typography>
+            {/* <CountdownTimer /> */}
         </Box>
     )
 }
