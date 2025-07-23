cf8be19247b8f1f5d42e65745acb8f2b659a2aa6
danibystrom <124156950+danibystrom@users.noreply.github.com>
2025-07-23 16:20:14 +0200

    Update image to  SplitSection component

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
       <HeroSection />
       <SplitSection title="DEN SOM VÄNTAR PÅ NÅGOT GOTT..." text="Vi hoppas att ni är lika exalterade som vi är, för nästa augusti händer det! Då vill vi fira kärleken, livet och varandra tillsammans med just er." text2="I augusti säger vi ja till varandra, och det skulle betyda allt att få dela den dagen med er. Därför vill vi redan nu skicka en kärleksfull påminnelse på att skriva in datumet i kalendern.
 
-Mer information kommer längre fram, vi längtar!" imageSrc="/IMG_3417.jpg" buttonText="Text för knapp" />
+Mer information kommer längre fram, vi längtar!" imageSrc="/garden.jpg" buttonText="Text för knapp" />
     </>
   );
 }
