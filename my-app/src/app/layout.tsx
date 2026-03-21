8b47155a653cea7c7bd94dbf68f9c9edbf0ed311
danibystrom <124156950+danibystrom@users.noreply.github.com>
2026-03-21 16:22:58 +0100

    Add @mui/material-nextjs package and integrate MuiAppProvider for improved layout management

--- my-app/src/app/layout.tsx
+++ my-app/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import dynamic from "next/dynamic";
 import { Italiana } from "next/font/google";
 import HeaderMenu from "./components/HeaderMenu";
+import MuiAppProvider from "./MuiAppProvider";
 import "./globals.css";
 
 const italiana = Italiana({ weight: "400", subsets: ["latin"], display: "swap" });
@@ -39,9 +40,11 @@ export default function RootLayout({
         />
       </head>
       <body suppressHydrationWarning>
-        <HeaderMenu />
-        {children}
-        <Footer />
+        <MuiAppProvider>
+          <HeaderMenu />
+          {children}
+          <Footer />
+        </MuiAppProvider>
       </body>
     </html>
   );
